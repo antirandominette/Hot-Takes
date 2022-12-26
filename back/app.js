64f0ab953543e893d31be11ebd08dfdbeba46346
Locholet <choletl@protonmail.com>
2022-12-26 16:36:28 +0100

    implemented express-rate-limiter on auth routes

--- back/app.js
+++ back/app.js
@@ -1,22 +1,33 @@
 require('dotenv').config(); 
 
-const env = process.env;
 const express = require('express');
 const mongoose = require('mongoose');
 const saucesRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 const path = require('path');
 const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+const app = express(); // Create an Express application
+const env = process.env;
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 20, // limit each IP to 20 requests per window
+    standardHeaders: true, 
+    legacyHeaders: true,
+    message: "Too many requests, please try again later"
+});
 
 mongoose.set('strictQuery', false); // To avoid the error: "strictQuery: true, strict: true, and strict mode are not compatible with each other"
 mongoose.connect(`mongodb+srv://${env.MONGO_USER_NAME}:${env.MONGO_USER_PASSWORD}@${env.MONGO_CLUSTER_ADDRESS}`, 
-    { useNewUrlParser: true, 
-    useUnifiedTopology: true }
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    }
 )
 .then(() => console.log('Connexion à MongoDB réussie !'))
-.catch(() => console.log('Connexion à MongoDB échouée !'));
+.catch(() => console.log('Connexion à MongoDB échouée ...'));
 
-const app = express(); // Create an Express application
 
 app.use(helmet(), helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // To avoid security issues && to avoid CORS errors
 app.use(express.json()); // Parse the request body to JSON
@@ -30,7 +41,7 @@ app.use((req, res, next) => { // To avoid CORS errors
 });
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/api/auth', userRoutes);
+app.use('/api/auth', authLimiter, userRoutes);
 app.use('/api/sauces', saucesRoutes); 
 
 module.exports = app;
